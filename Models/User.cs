95e5fe59abe5eefd7f48bb4b9cf69cbf2fb456db
mateus.cabral <mateuscabral7575@gmail.com>
2024-08-25 10:05:34 -0300

    Add Models

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.KmgShop.UserManager.Models;
+
+public class User
+{
+    public int UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public ICollection<Address> Address { get; set; }
+}
